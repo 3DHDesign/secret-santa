54e2f324072704ec7858cd53d8dc42a0c755d7c6
Tharuka Idushan <tharukaidushan@gmail.com>
2023-11-09 12:51:40 +0530

    make santa card

--- resources/views/livewire/user-dashboard.blade.php
+++ resources/views/livewire/user-dashboard.blade.php
@@ -1,7 +1,66 @@
-<div class="container">
+<div class="container-dashboard">
 
     <div class="santas-wrapper">
-        <div class="santa-card">Santa</div>
+        <div class="santa-card">
+            <div class="card-number">
+                <span>Select Me</span>
+            </div>
+            <img class="santa-image" src="{{ asset('assets/images/card/card-avatar.png') }}" alt="Santa Avatar">
+        </div>
+        <div class="santa-card">
+            <div class="card-number">
+                <span>Select Me</span>
+            </div>
+            <img class="santa-image" src="{{ asset('assets/images/card/card-avatar.png') }}" alt="Santa Avatar">
+        </div>
+        <div class="santa-card">
+            <div class="card-number">
+                <span>Select Me</span>
+            </div>
+            <img class="santa-image" src="{{ asset('assets/images/card/card-avatar.png') }}" alt="Santa Avatar">
+        </div>
+        <div class="santa-card">
+            <div class="card-number">
+                <span>Select Me</span>
+            </div>
+            <img class="santa-image" src="{{ asset('assets/images/card/card-avatar.png') }}" alt="Santa Avatar">
+        </div>
+        <div class="santa-card">
+            <div class="card-number">
+                <span>Select Me</span>
+            </div>
+            <img class="santa-image" src="{{ asset('assets/images/card/card-avatar.png') }}" alt="Santa Avatar">
+        </div>
+        <div class="santa-card">
+            <div class="card-number">
+                <span>Select Me</span>
+            </div>
+            <img class="santa-image" src="{{ asset('assets/images/card/card-avatar.png') }}" alt="Santa Avatar">
+        </div>
+        <div class="santa-card">
+            <div class="card-number">
+                <span>Select Me</span>
+            </div>
+            <img class="santa-image" src="{{ asset('assets/images/card/card-avatar.png') }}" alt="Santa Avatar">
+        </div>
+        <div class="santa-card">
+            <div class="card-number">
+                <span>Select Me</span>
+            </div>
+            <img class="santa-image" src="{{ asset('assets/images/card/card-avatar.png') }}" alt="Santa Avatar">
+        </div>
+        <div class="santa-card">
+            <div class="card-number">
+                <span>Select Me</span>
+            </div>
+            <img class="santa-image" src="{{ asset('assets/images/card/card-avatar.png') }}" alt="Santa Avatar">
+        </div>
+        <div class="santa-card">
+            <div class="card-number">
+                <span>Select Me</span>
+            </div>
+            <img class="santa-image" src="{{ asset('assets/images/card/card-avatar.png') }}" alt="Santa Avatar">
+        </div>
     </div>
 
     <style>
