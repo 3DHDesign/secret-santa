d09fee002ec92c98e9d722ff5d090b298ae269d6
Tharuka Idushan <tharukaidushan@gmail.com>
2023-11-17 09:39:20 +0530

    update mail template

--- resources/views/emails/verification.blade.php
+++ resources/views/emails/verification.blade.php
@@ -2,71 +2,165 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
-    <title>Secret Santa | 3DH</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Static Template</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet" />
 </head>
 
-<body>
+<body
+    style="
+      margin: 0;
+      font-family: 'Poppins', sans-serif;
+      background: #ffffff;
+      font-size: 14px;
+    ">
+    <div
+        style="
+        max-width: 680px;
+        margin: 0 auto;
+        padding: 45px 30px 60px;
+        background: #008049;
+       
+        background-repeat: no-repeat;
+        background-size: 800px 452px;
+        background-position: top center;
+        font-size: 14px;
+        color: #434343;
+      ">
+        <header>
+            <table style="width: 100%;">
+                <tbody>
+                    <tr style="height: 0;">
+                        <td>
+                            <img alt="" src="https://santa.3dhdesign.com/assets/images/secret-santa-logo.png"
+                                height="60px" />
+                        </td>
+                        <td style="text-align: right;">
+                            <span style="font-size: 16px; line-height: 30px; color: #ffffff;">FROM SANTA</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </header>
 
-    <div class="snowflake">
-        ❅
-    </div>
-    <div class="snowflake">
-        ❅
-    </div>
-    <div class="snowflake">
-        ❆
-    </div>
-    <div class="snowflake">
-        ❄
-    </div>
-    <div class="snowflake">
-        ❅
-    </div>
-    <div class="snowflake">
-        ❆
-    </div>
-    <div class="snowflake">
-        ❄
-    </div>
-    <div class="snowflake">
-        ❅
-    </div>
-    <div class="snowflake">
-        ❆
-    </div>
-    <div class="snowflake">
-        ❄
-    </div>
-    <div class="container">
-        <div class="login_wrapper">
-            <form>
-                <img class="form-santa-logo" src="{{ asset('assets/images/secret santa logo.png') }}" alt="Santa Logo">
-                <div class="form-heading">Password Reset Code</div>
-                <div class="submit-btn">{{ $data['code'] }}</div>
-                <p class="form-bottom-link">
+        <main>
+            <div
+                style="
+            margin: 0;
+            margin-top: 70px;
+            padding: 92px 30px 115px;
+            background: #ffffff;
+            border-radius: 30px;
+            text-align: center;
+          ">
+                <div style="width: 100%; max-width: 489px; margin: 0 auto;">
+                    <h1
+                        style="
+                margin: 0;
+                font-size: 24px;
+                font-weight: 500;
+                color: #1f1f1f;
+              ">
+                        Your OTP
+                    </h1>
+                    <p
+                        style="
+                margin: 0;
+                margin-top: 17px;
+                font-size: 16px;
+                font-weight: 500;
+              ">
+                        Hey {{ $data['name'] }},
+                    </p>
+                    <p
+                        style="
+                margin: 0;
+                margin-top: 17px;
+                font-weight: 500;
+                letter-spacing: 0.56px;
+              ">
+                        Use the following OTP
+                        to complete the procedure to change your account password. OTP is
+                        valid for
+                        <span style="font-weight: 600; color: #1f1f1f;">5 minutes</span>.
+                        Do not share this code with others.
+                    </p>
+                    <p
+                        style="
+                margin: 0;
+                margin-top: 60px;
+                font-size: 40px;
+                font-weight: 600;
+                letter-spacing: 25px;
+                color: #ba3d4f;
+              ">
+                        {{ $data['code'] }}
+                    </p>
+                </div>
+            </div>
 
-                </p>
-            </form>
+            <p
+                style="
+            max-width: 400px;
+            margin: 0 auto;
+            margin-top: 90px;
+            text-align: center;
+            font-weight: 500;
+            color: #8c8c8c;
+          ">
+                Need help? Ask at
+                <a href="mailto:santa@3dhdesign.com"
+                    style="color: #499fb6; text-decoration: none;">santa@3dhdesign.com</a>
+                or visit our
+                <a href="" target="_blank" style="color: #499fb6; text-decoration: none;">Help Center</a>
+            </p>
+        </main>
 
-        </div>
+        <footer
+            style="
+          width: 100%;
+          max-width: 490px;
+          margin: 20px auto 0;
+          text-align: center;
+          border-top: 1px solid #e6ebf1;
+        ">
+            <p
+                style="
+            margin: 0;
+            margin-top: 40px;
+            font-size: 16px;
+            font-weight: 600;
+            color: #434343;
+          ">
+                Secret Santa | 3DH Design
+            </p>
+            <p style="margin: 0; margin-top: 8px; color: #434343;">
+                No.12 Siri Dhamma Mawatha, Colombo 00700.
+            </p>
+            <div style="margin: 0; margin-top: 16px;">
+                <a href="" target="_blank" style="display: inline-block;">
+                    <img width="36px" alt="Facebook"
+                        src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661502815169_682499/email-template-icon-facebook" />
+                </a>
+                <a href="" target="_blank" style="display: inline-block; margin-left: 8px;">
+                    <img width="36px" alt="Instagram"
+                        src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661504218208_684135/email-template-icon-instagram" /></a>
+                <a href="" target="_blank" style="display: inline-block; margin-left: 8px;">
+                    <img width="36px" alt="Twitter"
+                        src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503043040_372004/email-template-icon-twitter" />
+                </a>
+                <a href="" target="_blank" style="display: inline-block; margin-left: 8px;">
+                    <img width="36px" alt="Youtube"
+                        src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503195931_210869/email-template-icon-youtube" /></a>
+            </div>
+            <p style="margin: 0; margin-top: 16px; color: #434343;">
+                Copyright © 2023 3DH Design | Secret Santa. All rights reserved.
+            </p>
+        </footer>
     </div>
-
-
-    <img class="floating-santa" src="{{ asset('assets/images/santa-with-baloon.png') }}" alt="Santa floating">
-    <img class="moon" src="{{ asset('assets/images/moon.png') }}" alt="Moon">
-    <img class="dh-office" src="{{ asset('assets/images/3dh-house.png') }}" alt="3dh-office">
-
-    @livewireScripts
-
 </body>
 
 </html>
