5989556232b797ad4efece12f5763ddac0984142
Tharuka Idushan <tharukaidushan@gmail.com>
2023-11-21 14:39:12 +0530

    change exel columns

--- resources/views/exports/secretSanta.blade.php
+++ resources/views/exports/secretSanta.blade.php
@@ -1,17 +1,20 @@
 <table>
     <thead>
         <tr>
-            <th>Player Full Name</th>
-            <th>to</th>
             <th>Selected Person</th>
-            <th>Player Mobile Number</th>
             <th>Selected Person Division</th>
+
+            <th>to</th>
+            <th>Player Full Name</th>
+            <th>Selected Person Division</th>
+            <th>Player Mobile Number</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($guests as $guest)
             <tr>
                 <td>{{ $guest->full_name }}</td>
+                <td>{{ $guest->division->name }}</td>
                 <td>to</td>
                 @php
                     $player_details = App\Models\GamePool::where('guest_id', $guest->id)
@@ -20,8 +23,8 @@
                 @endphp
                 @if (!$player_details == '')
                     <td>{{ $player_details->player->full_name }}</td>
-                    <td>{{ $player_details->player->number }}</td>
                     <td>{{ $player_details->player->division->name }}</td>
+                    <td>{{ $player_details->player->number }}</td>
                 @else
                     <td>NO</td>
                     <td>NO</td>
